1962d8126fa2052c60cbc78289af539428a29403
Rashmi Aribenchi <raribenchi@bunnings.com.au>
2023-05-05 04:34:53 +0800

    added my github repo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App"> React Weather App
+    <div className="App"> 
+    <h1>React Weather App </h1>
     <footer>
-      This project is coded by Rashmi Aribenchi and is 
+      This project is coded by <a href="https://ra-responsive-portfolio-v2.netlify.app"> Rashmi Aribenchi</a> and is {" "}
       <a 
       href="https://github.com/raribencchi/my-react-app" 
-      target="_blank"></a>
-      Open sourced on GitHub.
+      target="_blank">Open sourced on GitHub</a>
+      
     </footer>
     </div>
   );
