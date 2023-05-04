910655e5d7914bae04442146d77ebba6730a1c51
Rashmi Aribenchi <raribenchi@bunnings.com.au>
2023-05-05 04:25:20 +0800

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App"> React Weather App</div>
+    <div className="App"> React Weather App
+    <footer>
+      This project is coded by Rashmi Aribenchi and is 
+      <a 
+      href="https://github.com/raribencchi/my-react-app" 
+      target="_blank"></a>
+      Open sourced on GitHub.
+    </footer>
+    </div>
   );
 }
