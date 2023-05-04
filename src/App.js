4813f9a1d9a8786a45fa8a5514064b12488b9e75
Rashmi Aribenchi <raribenchi@bunnings.com.au>
2023-05-05 05:05:46 +0800

    installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App"> 
+    <div className='Container'>
     <h1>React Weather App </h1>
     <footer>
       This project is coded by <a href="https://ra-responsive-portfolio-v2.netlify.app"> Rashmi Aribenchi</a> and is {" "}
       <a href="https://github.com/raribencchi/my-react-app" target="blank"> Open sourced on GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
