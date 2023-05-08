4728b0346327968896db46dada4250bebc6232ed
Rashmi Aribenchi <raribenchi@bunnings.com.au>
2023-05-08 18:30:13 +0800

    Updated api key

--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const apiKey = "49e5da6325b173bba5c08dae7a5eba79";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
