1326f0b8040591d99ba44d84529bfd20057f9f22
Rashmi Aribenchi <raribenchi@bunnings.com.au>
2023-05-09 15:17:27 +0800

    added default city

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App"> 
     <div className='Container'>
     <h1>React Weather App </h1>
-    <Weather />
+    <Weather defaultCity="Paris" />
     <footer>
       This project is coded by <a href="https://ra-responsive-portfolio-v2.netlify.app"> Rashmi Aribenchi</a> and is {" "}
       <a href="https://github.com/raribencchi/my-react-app" target="blank"> Open-sourced on GitHub and hosted on Netlify.</a>
